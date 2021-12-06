952151be57840abb5a0539b61f369d5177bf7f29
saisri <saisrikrish2001@gmail.com>
2021-12-06 20:49:01 +0530

    first commit

---
+++ wms/includes/header.php
@@ -0,0 +1,38 @@
+<!--header-->
+            <div class="header" style="background-color: #242D0F;">
+                <div class="container">
+                    <div class="header-top" style="background-color: #242D0F;">
+                        <nav class="navbar navbar-default" style="background-color: #242D0F;">
+                            <div class="container-fluid" style="background-color: #242D0F;">
+    <!-- Brand and toggle get grouped for better mobile display -->
+                                <div class="navbar-header">
+                                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                      </button>
+                                    <div class="navbar-brand" style="background-color: #242D0F;">
+                                        <h1><a href="index.php">Wildlife Management System</a></h1>
+                                    </div>
+                                </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                                    <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="about.php">About</a></li>
+                                    <li><a href="contact.php">Contact</a>
+                                    </li>
+                                    <li><a href="animals.php">Animals</a>
+                                    </li>
+                                    <li><a href="admin/index.php">Admin</a></li>
+                                </ul>
+                              
+                            </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+                        </nav>
+
+                    </div>
+                </div>
+            </div>
